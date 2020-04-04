04ed54bde549fba14d80408848c5b4a5cb7ffb04
Taha Shabani <taha.shabani.m@gmail.com>
2020-04-04 19:26:06 +0430

    a simple router added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App2 from './App2'
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+ReactDOM.render((
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' component={App2} />
+      <Route path='/app1' component={App} />
+    </div>
+</BrowserRouter>
+), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
