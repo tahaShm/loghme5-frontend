acaef69f4dc39cbf8c876607d6724531d3c4996c
Taha Shabani <taha.shabani.m@gmail.com>
2020-04-05 14:34:14 +0430

    folder structures added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import App2 from './App2'
+import App from './screens/App';
+import App2 from './screens/App2'
 import * as serviceWorker from './serviceWorker';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.render((
   <BrowserRouter>
     <div>
-      <Route exact path='/' component={App2} />
-      <Route path='/app1' component={App} />
+      <Route exact path='/' component={App} />
+      <Route path='/app2' component={App2} />
     </div>
 </BrowserRouter>
 ), document.getElementById('root'))
